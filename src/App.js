7da52d5850a150ac8abdda65b05ba59c5819c4b2
Eugenio Espinoza <68721930+egitweb@users.noreply.github.com>
2021-03-07 00:03:43 -0600

    start up

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Wrapper from "./components/Wrapper";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/home" component={Home} />
+        </Wrapper>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
